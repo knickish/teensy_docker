d777beccebad47c2fd2ac808956bc7cf0e86f066
kirk <knickish@gmail.com>
2021-05-30 12:28:20 -0700

    fixed inclusion of some libraries with unusual directory structure

--- internal/internal_scripts/libs_cmake.py
+++ internal/internal_scripts/libs_cmake.py
@@ -26,7 +26,8 @@ def generate_lib_cmake(supported, conflicts, lib_name):
     ret = f'\nIF({ifstrProcessed})'
     ret+= f'\nfile(GLOB {lib_name}_sources CONFIGURE_DEPENDS "*.h" "*.cpp" "*.c++" "*.c" "*.hpp" '
     ret+= '"src/*.h" "src/*.cpp" "src/*.c++" "src/*.c" "src/*.hpp" '
-    ret+= '"utility/*.h" "utility/*.cpp" "utility/*.c++" "utility/*.c" "utility/*.hpp")'
+    ret+= '"utility/*.h" "utility/*.cpp" "utility/*.c++" "utility/*.c" "utility/*.hpp" '
+    ret+= '"src/utility/*.h" "src/utility/*.cpp" "src/utility/*.c++" "src/utility/*.c" "src/utility/*.hpp")'
     ret+= f"\nadd_library({lib_name} STATIC ${{{lib_name}_sources}})"
     ret+= f"\nset_target_properties({lib_name} PROPERTIES LINKER_LANGUAGE CXX)"
     ret+= f"\ntarget_include_directories({lib_name} PUBLIC ${{CMAKE_CURRENT_LIST_DIR}})"
@@ -37,21 +38,41 @@ def generate_lib_cmake(supported, conflicts, lib_name):
 def generate_lib_top_cmake(config):
     ret = """message(STATUS "${CMAKE_CURRENT_LIST_DIR}")
             \nSUBDIRLIST(SUBDIRS ${CMAKE_CURRENT_LIST_DIR})
+            \nlist(APPEND EXTRA_INCLUDE_LIBS_INNER)
+            \nlist(APPEND EXTRA_LINK_LIBS_INNER)
             \nFOREACH(subdir ${SUBDIRS})
-            \n\tinclude_directories(${subdir})
-            \n\tinclude_directories(${subdir}/src)
-            \n\tinclude_directories(${subdir}/utility)
+            \n\tinclude_directories(${CMAKE_CURRENT_LIST_DIR}/${subdir})
+            \n
+            \n\tIF(EXISTS "${CMAKE_CURRENT_LIST_DIR}/${subdir}/src" AND IS_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/${subdir}/src")
+            \n\t\tmessage(STATUS "${subdir}/src")
+            \n\t\tinclude_directories(${CMAKE_CURRENT_LIST_DIR}/${subdir}/src)
+            \n\t\tlist(APPEND EXTRA_INCLUDE_LIBS_INNER ${CMAKE_CURRENT_LIST_DIR}/${subdir}/src;)
+            \n\tENDIF()
+            \n
+            \n\tIF(EXISTS "${CMAKE_CURRENT_LIST_DIR}/${subdir}/utility" AND IS_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/${subdir}/utility")
+            \n\t\tmessage(STATUS "${subdir}/utility")
+            \n\t\tinclude_directories(${CMAKE_CURRENT_LIST_DIR}/${subdir}/utility)
+            \n\t\tlist(APPEND EXTRA_INCLUDE_LIBS_INNER ${CMAKE_CURRENT_LIST_DIR}/${subdir}/utility;)
+            \n\tENDIF()
+            \n
+            \n\tIF(EXISTS "${CMAKE_CURRENT_LIST_DIR}/${subdir}/src/utility" AND IS_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/${subdir}/src/utility")
+            \n\t\tmessage(STATUS "${subdir}/src/utility")
+            \n\t\tinclude_directories(${CMAKE_CURRENT_LIST_DIR}/${subdir}/src/utility)
+            \n\t\tlist(APPEND EXTRA_INCLUDE_LIBS_INNER ${CMAKE_CURRENT_LIST_DIR}/${subdir}/src/utility;)
+            \n\tENDIF()
             \nendforeach()
-            \nlist(APPEND EXTRA_LIBS_INNER)
+
             \nSUBDIRLIST(SUBDIRS ${CMAKE_CURRENT_LIST_DIR})
             \nFOREACH(subdir ${SUBDIRS})
             \nset(LIBNAME "")
             \nmessage(STATUS "${subdir}")
             \n\tadd_subdirectory(${subdir})
-            \nlist(APPEND EXTRA_LIBS_INNER ${LIBNAME};)
+            \nlist(APPEND EXTRA_INCLUDE_LIBS_INNER ${LIBNAME};)
+            \nlist(APPEND EXTRA_LINK_LIBS_INNER ${LIBNAME};)
             \nendforeach()
-            \nset(EXTRA_LIBS ${EXTRA_LIBS_INNER} PARENT_SCOPE)
-            \nmessage(STATUS ${EXTRA_LIBS_INNER})"""
+            \nset(EXTRA_INCLUDE_LIBS ${EXTRA_INCLUDE_LIBS_INNER} PARENT_SCOPE)
+            \nset(EXTRA_LINK_LIBS ${EXTRA_LINK_LIBS_INNER} PARENT_SCOPE)
+            \nmessage(STATUS ${EXTRA_INCLUDE_LIBS_INNER})"""
             
     return ret
 
