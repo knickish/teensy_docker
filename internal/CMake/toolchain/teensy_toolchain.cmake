93eaa785b17985c8890e289a949f177d6f6158e6
kiwiDaveENZED <davekerry@gmail.com>
2022-05-29 13:01:09 +1200

    Add support for compiling assembler (.S) files (#3)

--- internal/CMake/toolchain/teensy_toolchain.cmake
+++ internal/CMake/toolchain/teensy_toolchain.cmake
@@ -6,6 +6,8 @@ set(CMAKE_CROSSCOMPILING 1)
 
 set(CMAKE_C_COMPILER arm-none-eabi-gcc) 
 set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
 
 SET (CMAKE_C_COMPILER_WORKS 1)
 SET (CMAKE_CXX_COMPILER_WORKS 1) # Required to make the previous line work for a target that requires a custom linker file
+SET (CMAKE_ASM_COMPILER_WORKS 1)
